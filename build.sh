0f74818760fdeeb5eb633f1cf1a48588c24e4e35
Thomas Vincent <vrince@gmail.com>
2022-05-03 14:57:33 -0400

    add pack

--- build.sh
+++ build.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-TAG="${1}"
+ARCH="${1}"
+TAG="${2}"
+
+echo "Arch(${ARCH})"
 
 if [[ -z ${TAG} || ${TAG} == "master" ]] ; then
     TAG="v1.6.9"
     echo "Using default tag(${TAG})"
 fi
 
-echo "Cloning RedisTimeSeries git tag(${TAG})"
+echo "Tag(${TAG})"
+echo "Cloning RedisTimeSeries .."
+
 git clone --quiet --single-branch --branch="${TAG}" --depth=1 --recursive "https://github.com/RedisTimeSeries/RedisTimeSeries.git"
 pushd "RedisTimeSeries"
 git checkout --quiet --detach "${TAG}"
 git describe --tags
 
+echo "Building ..."
 make setup
-make build
+make prefix=./install build
+
+echo "Packing ..."
+find . -type t -name "redis*.so" -exec gzip {} -c > "RedisTimeSeries-linux-${ARCH}-${TAG}.gz" \;
 
 popd
