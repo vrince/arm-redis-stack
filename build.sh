5deeb36e6f6329acbb2af592cab5e438a62c339c
Thomas Vincent <vrince@gmail.com>
2022-05-03 16:48:01 -0400

    add redis

--- build.sh
+++ build.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-ARCH="${1}"
-TAG="${2}"
+MODULE="${1}"
+ARCH="${2}"
+TAG="${3}"
 
+echo "🌿 Module(${MODULE})"
 echo "🦑 Arch(${ARCH})"
+echo "🏷️ Tag(${TAG})"
 
-if [[ -z ${TAG} || ${TAG} == "master" ]] ; then
-    TAG="v1.6.9"
-    echo "Using default tag(${TAG})"
+if [[ ${MODULE} == "redis-timeseries" ]] ; then
+
+    if [[ -z ${TAG} || ${TAG} == "master" ]] ; then
+        TAG="v1.6.9"
+        echo "Using default tag(${TAG})"
+    fi
+
+    echo "🐏 Cloning ..."
+    git clone --quiet --single-branch --branch="${TAG}" --depth=1 --recursive "https://github.com/RedisTimeSeries/RedisTimeSeries.git"
+    pushd "RedisTimeSeries"
+    git checkout --quiet --detach "${TAG}"
+    git describe --tags
+
+    echo "🔨 Building ..."
+    make setup
+    make build
+    popd
+
+    echo "🧊 Packing ..."
+    find . -type f -name "redis*.so" -exec gzip -v {} -c > "redis-timeseries-linux-${ARCH}-${TAG}.gz" \;
 fi
 
-echo "Tag(${TAG})"
-echo "🐏 Cloning RedisTimeSeries .."
+if [[ ${MODULE} == "redis" ]] ; then
 
-git clone --quiet --single-branch --branch="${TAG}" --depth=1 --recursive "https://github.com/RedisTimeSeries/RedisTimeSeries.git"
-pushd "RedisTimeSeries"
-git checkout --quiet --detach "${TAG}"
-git describe --tags
+fi
 
-echo "🔨 Building ..."
-make setup
-make build
-
-popd
-
-echo "🧊 Packing ..."
-find . -type f -name "redis*.so" -exec gzip -v {} -c > "redis-timeseries-linux-${ARCH}-${TAG}.gz" \;
+echo "✨ Done"
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 # arm-RedisTimeSeries
 
 ![](https://badgen.net/github/release/vrince/arm-RedisTimeSeries)
-[![CI](https://github.com/vrince/arm-RedisTimeSeries/actions/workflows/redis.yml/badge.svg)](https://github.com/vrince/arm-RedisTimeSeries/actions/workflows/redis.yml)
+
+[![redis](https://github.com/vrince/arm-RedisTimeSeries/actions/workflows/redis.yml/badge.svg)](https://github.com/vrince/arm-RedisTimeSeries/actions/workflows/redis.yml)
+[![redis-timeseries](https://github.com/vrince/arm-RedisTimeSeries/actions/workflows/redis-timeseries.yml/badge.svg)](https://github.com/vrince/arm-RedisTimeSeries/actions/workflows/redis-timeseries.yml)
 
 Automated builds of [RedisTimeSeries](https://github.com/RedisTimeSeries/RedisTimeSeries) for arm 32bits (armv6 / armv7). Primary target is raspberry-pi using 32 bits OS.
 
