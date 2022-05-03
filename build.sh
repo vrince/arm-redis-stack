c6d996055b7470f5064c4fb83e3a726a569d101c
Thomas Vincent <vrince@gmail.com>
2022-05-03 17:13:25 -0400

    add redis build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MODULE="${1}"
-ARCH="${2}"
+ARCH="${2:-x86_64}"
 TAG="${3}"
 
 echo "🌿 Module(${MODULE})"
@@ -32,6 +32,24 @@ fi
 
 if [[ ${MODULE} == "redis" ]] ; then
 
+    if [[ -z ${TAG} || ${TAG} == "master" ]] ; then
+        TAG="7.0"
+        echo "Using default tag(${TAG})"
+    fi
+
+    echo "🐏 Cloning ..."
+    git clone --quiet --single-branch --branch="${TAG}" --depth=1 "https://github.com/redis/redis.git"
+    pushd "redis"
+    git checkout --quiet --detach "${TAG}"
+    git describe --tags
+
+    echo "🔨 Building ..."
+    make clean
+    make PREFIX=${PWD}/install BUILD_TLS=yes USE_SYSTEMD=yes MALLOC=libc install
+    popd
+
+    echo "🧊 Packing ..."
+    tar -cvzf "redis-linux-${ARCH}-${TAG}.tar.gz" -C "./redis/install" .
 fi
 
 echo "✨ Done"
